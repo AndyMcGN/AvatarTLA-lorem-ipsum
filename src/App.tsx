a1c300f4073750d98dbd91115ec8acc8b472b618
Andy <andrew.mcginn@gmail.com>
2021-07-05 17:36:43 +0100

    padding issue fixed

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,9 @@
-import logo from './logo.svg';
 import './App.css';
 import Form from './components/Form';
 import TextGenerator from './components/TextGenerator';
-import React, { Component } from 'react';
+import { Component } from 'react';
 import './components/stylesheets/styles.css';
 import avatarLogo from './avatar-logo.png'
-
-interface State {
-  paragraphs: number;
-}
-
 
 export default class App extends Component {
 
@@ -29,7 +23,7 @@ export default class App extends Component {
         <div>
           <div className='header'>
             <div>
-              <img src={avatarLogo} /> 
+              <img src={avatarLogo} alt="avatar logo" /> 
               <h2>Lorem Ipsum Generator</h2>
               <h4>Lorem Ipsum generated from random avatar phrases.</h4>
             </div>
